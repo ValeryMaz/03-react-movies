8696db497f5040fd1875b959631abff5b026e874
ValeryMaz <mazhytova1996@gmail.com>
2025-07-14 16:00:21 -0400

    done

---
+++ src/MovieGrid/MovieGrid.tsx
@@ -0,0 +1,34 @@
+import css from "./MovieGrid.module.css";
+import type { Movie } from "../types/movies";
+
+export default function MovieGrid({
+  movies,
+  onSelect,
+}: {
+  movies: Movie[];
+  onSelect: (movie: Movie) => void;
+}) {
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+  return (
+    <ul className={css.grid}>
+      {movies.map((movie) => (
+        <li key={movie.id} onClick={() => onSelect(movie)}>
+          <div className={css.card}>
+            <img
+              className={css.image}
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : defaultImg
+              }
+              alt={movie.title}
+              loading="lazy"
+            />
+            <h2 className={css.title}>{movie.title}</h2>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
